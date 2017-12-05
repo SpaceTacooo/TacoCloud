888e742df943c9ff56cdecb9d9ed379b0f2c2c76
SpaceTaco <34287584+SpaceTacooo@users.noreply.github.com>
2017-12-06 09:44:15 +1000

    Add serverinfo

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ client.on('ready', () => {
 client.on('message', message => {
 
     if (message.content.toLowerCase() === `${prefix}help`) {
-      message.channel.send(`__SpaceTaco Commands__:\n\n  **${prefix}help**\n  Shows a help menu with a list of commands.`);
+      message.channel.send(`__SpaceTaco Commands__:\n\n  **${prefix}help**\n  Shows a help menu with a list of commands.\n\n  **${prefix}serverinfo**\n  Display information about the server the command is run in.`);
+    }
+    
+    if (message.content.toLowerCase() === `${prefix}serverinfo`) {
+        if (message.guild === false) return (`The bot hit an error while performing this command:\nGuild not available. This means the guild is not able to provide data right now.`);
+        message.channel.send(`__Server Information__:\n\n  **Name**\n  ${message.guild.name}\n\n  **Owner**\n  ${message.guild.owner.tag} (${message.guild.owner.id})\n\n  **Members**\n  ${message.guild.members.size}`);       
     }
     
 });
