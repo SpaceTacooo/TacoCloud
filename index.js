4249435ed4059c17eadf94bdc6a6ca313561115f
SpaceTaco <34287584+SpaceTacooo@users.noreply.github.com>
2017-12-06 09:05:56 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('SpaceTaco logged in!!');
+});
+
+client.on('message', message => {
+
+    if (message.content.toLowerCase() === `${prefix}help`) {
+      message.channel.send(`__SpaceTaco Commands__:\n\n  **${prefix}help**\n  Shows a help menu with a list of commands.`);
+    }
+    
+});
+
+client.login( token, duh );
