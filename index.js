b2f1c32f492afb2491b32ba36fc67aa4875f5cde
SpaceTaco <34287584+SpaceTacooo@users.noreply.github.com>
2017-12-06 09:49:48 +1000

    Fix serverinfo + add more content to serverinfo

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const client = new Discord.Client();
     var tags = data.tags;
 
 client.on('ready', () => {
-    console.log('SpaceTaco logged in!!');
+    console.log('TacoCloud logged in!!');
 });
 
 client.on('message', message => {
 
     if (message.content.toLowerCase() === `${prefix}help`) {
-      message.channel.send(`__SpaceTaco Commands__:\n\n  **${prefix}help**\n  Shows a help menu with a list of commands.\n\n  **${prefix}serverinfo**\n  Display information about the server the command is run in.`);
+      message.channel.send(`__TacoCloud Commands__:\n\n  **${prefix}help**\n  Shows a help menu with a list of commands.\n\n  **${prefix}serverinfo**\n  Display information about the server the command is run in.`);
     }
     
     if (message.content.toLowerCase() === `${prefix}serverinfo`) {
         if (message.guild === false) return (`The bot hit an error while performing this command:\nGuild not available. This means the guild is not able to provide data right now.`);
-        message.channel.send(`__Server Information__:\n\n  **Name**\n  ${message.guild.name}\n\n  **Owner**\n  ${message.guild.owner.tag} (${message.guild.owner.id})\n\n  **Members**\n  ${message.guild.members.size}`);       
+        message.channel.send(`__Server Information__:\n\n  **Name**\n  ${message.guild.name}\n\n  **Owner**\n  ${message.guild.owner.user.tag} (${message.guild.owner.id})\n\n  **Members (inc. bots)**\n  ${message.guild.members.size}\n\n  **Region**\n  ${message.guild.region}\n\n  **Created**\n  ${message.guild.createdAt}
     }
     
 });
